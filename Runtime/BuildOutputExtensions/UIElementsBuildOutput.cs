064ce1fc3a21c3c7fe25a000506a34bd5f57d7c3
sxm-sxpxxl <alescha.pestov@yandex.ru>
2024-07-08 22:51:28 +0300

    Optimized vertices conversion to specified UI format (#3)

--- Runtime/BuildOutputExtensions/UIElementsBuildOutput.cs
+++ Runtime/BuildOutputExtensions/UIElementsBuildOutput.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,27 +9,44 @@ namespace Sxm.UIFactory
 {
     public static class UIElementsBuildOutput
     {
-        public readonly struct Result
+        public readonly struct UIElementsResult : IDisposable
         {
             public readonly Vertex[] Vertices;
             public readonly ushort[] Indices;
 
-            public Result(Vertex[] vertices, ushort[] indices)
+            private readonly ArrayPool<Vertex> _verticesPool;
+
+            public UIElementsResult(Vector2[] vertices, Color[] tintColors, ushort[] indices)
             {
-                Vertices = vertices;
+                (Vertices, _verticesPool) = GetVertices(vertices, tintColors);
                 Indices = indices;
+            }
+
+            private static (Vertex[] result, ArrayPool<Vertex> pool) GetVertices(Vector2[] vertices, Color[] tintColors)
+            {
+                var pool = ArrayPool<Vertex>.Shared;
+                var result = pool.Rent(vertices.Length);
+
+                for (var i = 0; i < vertices.Length; i++)
+                {
+                    result[i] = new Vertex
+                    {
+                        position = new Vector3(vertices[i].x, vertices[i].y, Vertex.nearZ),
+                        tint = tintColors[i]
+                    };
+                }
+
+                return (result, pool);
+            }
+
+            public void Dispose()
+            {
+                _verticesPool.Return(Vertices);
             }
         }
 
-        public static IEnumerable<Result> GetDataForUIElements(this UIFactoryManager.CookedMesh input) => input.Result.GetDataForUIElements();
+        public static IEnumerable<UIElementsResult> GetUIElementsResults(this UIFactoryManager.CookedMesh input) => input.Result.GetUIElementsResults();
 
-        private static IEnumerable<Result> GetDataForUIElements(this IEnumerable<MeshData> input) => input.Select(mesh => new Result(
-            vertices: mesh.Vertices.Select((v, vIndex) => new Vertex
-            {
-                position = new Vector3(v.x, v.y, Vertex.nearZ),
-                tint = mesh.TintColors[vIndex]
-            }).ToArray(),
-            indices: mesh.Indices
-        ));
+        private static IEnumerable<UIElementsResult> GetUIElementsResults(this IEnumerable<MeshData> input) => input.Select(mesh => new UIElementsResult(mesh.Vertices, mesh.TintColors, mesh.Indices));
     }
 }
