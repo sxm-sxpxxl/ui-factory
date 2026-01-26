6d40b97c99cf13a5b85220760d195f7a31a14f97
sxm-sxpxxl <alescha.pestov@yandex.ru>
2026-01-27 00:26:53 +0300

    polish: removed redundant comments [TASK-14]

--- Runtime/Components/Lines/SolidLineMeshBuilder.cs
+++ Runtime/Components/Lines/SolidLineMeshBuilder.cs
@@ -4,10 +4,8 @@ namespace Sxm.UIFactory.Components.Lines
 {
     internal sealed class SolidLineMeshBuilder : MeshBuilder<SolidLineMeshDescription>
     {
-        // todo@sxm: add description validators through attributes?
         protected override IEnumerable<MeshData> Build(SolidLineMeshDescription description)
         {
-            // todo@sxm: use nullable value types? and validation before use?
             yield return MeshUtils.CreateLineMesh(description.StartPosition, description.EndPosition, description.Thickness, description.Color);
         }
     }
