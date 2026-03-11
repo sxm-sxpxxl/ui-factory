99438385b75a5f32195f26da21d8c13feb935e9d
sxm-sxpxxl <alescha.pestov@yandex.ru>
2026-03-12 01:10:16 +0300

    minor: added some comments & attributes [TASK-18]

--- Runtime/Components/Utils/CompilerServicesResolver.cs
+++ Runtime/Components/Utils/CompilerServicesResolver.cs
@@ -1,5 +1,8 @@
-﻿namespace System.Runtime.CompilerServices
+﻿using JetBrains.Annotations;
+
+namespace System.Runtime.CompilerServices
 {
+    [UsedImplicitly]
     internal static class IsExternalInit
     {
     }
